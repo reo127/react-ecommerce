fbb382c5f7fac2313ec6d57b02a225eeb7427426
Arathi <arathi_gasti@bluerose-tech.com>
2024-06-22 12:32:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import  Navbar  from "./components/Navbar"
+import  Spinner  from "./components/Spinner"
+import ModalSearch  from "./components/ModalSearch"
+import  Home  from "./components/Home"
+import Featurs  from "./components/Featurs"
+import Fruits  from "./components/Fruits"
+import Vesitable  from "./components/Fruits"
+import Footer from "./components/Footer"
+
+
+function App() {
+
+
+  return (
+    <>
+
+    {/* <Spinner/> */}
+    <Navbar/>
+    <ModalSearch/>
+    <Home/>
+    <Featurs/>
+    <Fruits/>
+    <Vesitable/>
+    <Footer/>
+
+      
+    </>
+  )
+}
+
+export default App
